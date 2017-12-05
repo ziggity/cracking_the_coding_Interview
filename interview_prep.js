22f105a2da045c423b051feb33f266ce4cc52f03
Zach <17103355+ziggity@users.noreply.github.com>
2017-12-05 12:05:09 -0800

    avanade prep

---
+++ interview_prep.js
@@ -0,0 +1,24 @@
+// Tell me about a situation where something did not go right, and how did you handle it?
+
+//
+
+//Tell me about a project that you worked on where you didn't have the expertise necessary and how you overcame that and finished the project?  
+
+// Google, youtube, stackoverflow, ask a mentor last case after I did my due dilligence. 
+
+// What are the 3 major components of Object Oriented  programming? 
+
+// Abstraction, Encapsulation, Inheritance and Polymorphism.
+
+// what's WCF? 
+
+// Windows Communication Foundation.
+
+// what do you know about avanade?
+
+//Avanade was founded in 2000 as a joint venture between Accenture and Microsoft to serve an untapped market for Microsoft-focused consulting services
+
+// who's the ceo?
+
+// Adam Warby for the past 10 yrs
+
